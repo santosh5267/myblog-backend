823452f358e23ab1f77dd7aae46099c0ae7f00cc
santosh5266 <santoshkumar.k@itpeoplecorp.com>
2023-05-30 12:40:02 +0530

    git ignore for ignore mongo logs

--- src/server.js
+++ src/server.js
@@ -5,10 +5,12 @@ app.use(express.json());
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: []
 },
 {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }
 ]
 app.get('/hello', (req, res) => {
@@ -20,7 +22,7 @@ app.get('/hello/:name', (req, res) => {
 })
  
 app.put('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
+   
     const  { name } = req.params;
    const article =  articlesInfo.find(a=> a.name === name);
    if(article){
@@ -28,5 +30,19 @@ app.put('/api/articles/:name/upvote', (req, res) => {
    res.send( `The ${name} article now has ${article.upvotes} upvotes`);
    } 
 });  
+
+app.post('/api/articles/:name/comments',(req,res)=>{
+    const name  = req.params;
+    const {postedBy, text} =  req.body;
+    
+    const article  = articlesInfo.find(a => a.name === name);
+
+    if(article){
+    article.comments.push({postedBy,text});
+    res.send(article.comments);
+    }else{
+        res.send('Article doesnot exist');
+    }
+});
 
 app.listen(8000, () => console.log('Server is listening on port 8000'));
